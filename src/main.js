ddd231a39736d2a200080ee5731110c10bbe052e
307025293@qq.com <huchunyan123>
2018-07-25 21:38:35 +0800

    首页搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入路由模块
+import VueRouter from "vue-router"
+//引入index组件
+import index from './components/index.vue';
+//引入商品详情commodity组件
+import commodity from './components/commodity.vue';
+
+//引入css样式
+import './assets/statics/site/css/style.css';
+//使用路由中间件
+Vue.use(VueRouter);
+
+//注册路由规则
+const router = new VueRouter({
+  routes: [
+    {path:'/',component:index},
+    {path:'/index',component:index},
+    {path:'/commodity',component:commodity}
+  ]
+
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  el:"#app",
+  //挂载到vue
+  router,
   render: h => h(App)
-}).$mount('#app')
+})
